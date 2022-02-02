427f30c4bf2913e2ed093a062909f40ae52cdce3
Nick McSpadden <nmcspadden@gmail.com>
2022-02-02 11:08:36 -0800

    Improved output from AugmentMe

--- AugmentMe.js
+++ AugmentMe.js
@@ -121,7 +121,13 @@ export async function main(ns) {
 			await promptForAugs(ns, aug_map, Object.fromEntries(desired_augs), flagdata.ask, flagdata.buy)
 		}
 	} else {
-		ns.tprint("Augs to aim for: " + Object.keys(Object.fromEntries(desired_augs)).join(", "));
+		let sorted_exp_augs = Object.keys(exp_augs).sort((a, b) => a["cost"] - b["cost"]).reverse();
+		ns.tprint("Augs with +exp stats sorted by cost: " + sorted_exp_augs.join(", "));
+		let sorted_desired_augs = Object.keys(Object.fromEntries(desired_augs)).sort((a, b) => a["cost"] - b["cost"]).reverse();
+		ns.tprint("Augs to aim for with your desired stats: " + sorted_desired_augs.join(", "));
+		ns.tprint(`Most expensive aug is ${sorted_desired_augs[0]} at ${ns.nFormat(Object.fromEntries(desired_augs)[sorted_desired_augs[0]]["cost"], '$0.00a')}`);
+		let most_rep_aug = Object.keys(Object.fromEntries(desired_augs)).sort((a, b) => a["repreq"] - b["repreq"]).reverse()[0];
+		ns.tprint(`Most rep-required aug is ${most_rep_aug} at ${ns.nFormat(Object.fromEntries(desired_augs)[most_rep_aug]["repreq"], '0.000a')}`);
 	}
 }
 
