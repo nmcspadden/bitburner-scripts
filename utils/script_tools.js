9ea463f77c86fd0996df065f53f540c574b34aad
Nick McSpadden <nmcspadden@gmail.com>
2022-02-08 10:34:44 -1000

    script_tools can run on home if not on target

--- utils/script_tools.js
+++ utils/script_tools.js
@@ -2,10 +2,21 @@
  * @param {NS} ns 
  * @param {string} script Name of script to evaluate and run
  * @param {string} host Name of server to execute on
+ * @param {boolean} run_on_home If the server does not have adequate RAM, run on home instead
 */
-export function maximizeScriptUse(ns, script, host) {
+export function maximizeScriptUse(ns, script, host, run_on_home=false) {
 	let current_ram = ns.getServerMaxRam(host) - ns.getServerUsedRam(host);
+	// ns.tprint(`${host} current RAM: ${current_ram}`);
 	let script_cost = ns.getScriptRam(script);
-	let threads = Math.floor(current_ram / script_cost); 
-	ns.exec(script, host, threads);
+	// ns.tprint(`${host} script cost: ${script_cost}`);
+	let threads = Math.floor(current_ram / script_cost);
+	// ns.tprint(`${host} threads: ${threads}`);
+	if (threads > 0) {
+		// Kill it first before recalculating usage
+		ns.kill(script, host);
+		ns.exec(script, host, threads);
+	} else {
+		ns.kill(script, 'home', host);
+		ns.exec(script, 'home', 1, host);
+	}
 }
