efced13bbf1525e4e85ccf2cf5b07c68b57cc6ba
Nick McSpadden <nmcspadden@gmail.com>
2022-02-07 18:45:57 -1000

    Added serverGrower.js

---
+++ serverGrower.js
@@ -0,0 +1,20 @@
+// Server Grower (c) 2022 Tyrope
+// https://github.com/tyrope/bitburner/blob/master/serverGrower.js
+// Usage: run serverGrower.js [target]
+// parameter target: The server to grow.
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    // Infinite loop, go!
+    while (true) {
+        // Make sure we're not fighting unneeded security.
+        while (ns.getServerSecurityLevel(tgt) > ns.getServerMinSecurityLevel(tgt)) {
+            await ns.weaken(tgt);
+        }
+        if (ns.getServerMoneyAvailable(tgt) < ns.getServerMaxMoney(tgt)) {
+            await ns.grow(tgt);
+        } else {
+            break;
+        }
+    }
+}
