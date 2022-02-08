5be5a1e1ccc49d53b2131e42d76af8bf2cbb31b7
Nick McSpadden <nmcspadden@gmail.com>
2022-02-08 10:54:17 -1000

    script_tools outsources its max thread count

--- utils/script_tools.js
+++ utils/script_tools.js
@@ -2,14 +2,9 @@
  * @param {NS} ns 
  * @param {string} script Name of script to evaluate and run
  * @param {string} host Name of server to execute on
- * @param {boolean} run_on_home If the server does not have adequate RAM, run on home instead
 */
-export function maximizeScriptUse(ns, script, host, run_on_home=false) {
-	let current_ram = ns.getServerMaxRam(host) - ns.getServerUsedRam(host);
-	// ns.tprint(`${host} current RAM: ${current_ram}`);
-	let script_cost = ns.getScriptRam(script);
-	// ns.tprint(`${host} script cost: ${script_cost}`);
-	let threads = Math.floor(current_ram / script_cost);
+export function maximizeScriptUse(ns, script, host) {
+	let threads = maxThreads(ns, script, host);
 	// ns.tprint(`${host} threads: ${threads}`);
 	if (threads > 0) {
 		// Kill it first before recalculating usage
@@ -19,4 +14,12 @@ export function maximizeScriptUse(ns, script, host, run_on_home=false) {
 		ns.kill(script, 'home', host);
 		ns.exec(script, 'home', 1, host);
 	}
+}
+
+export function maxThreads(ns, script, host) {
+	let current_ram = ns.getServerMaxRam(host) - ns.getServerUsedRam(host);
+	// ns.tprint(`${host} current RAM: ${current_ram}`);
+	let script_cost = ns.getScriptRam(script);
+	// ns.tprint(`${host} script cost: ${script_cost}`);
+	return Math.floor(current_ram / script_cost);
 }
