db12780b111dbbde74b8777bd2115aee3c2d5367
Nick McSpadden <nmcspadden@gmail.com>
2022-02-10 17:58:22 -1000

    starter works correctly now

--- starter.js
+++ starter.js
@@ -7,9 +7,9 @@ const HOME = 'home';
 /** @param {NS} ns **/
 export async function main(ns) {
 	// Create network map
-	ns.spawn("utils/networkmap.js", HOME);
-	// Create network map
-	ns.exec("growHackingXP.js", HOME);
+	ns.exec("utils/networkmap.js", HOME);
+	// // Grow hacking XP on joesguns
+	// ns.exec("growHackingXP.js", HOME);
 	// Start early game plan
-	ns.exec("earlygameplan.js", HOME);
+	ns.spawn("earlygameplan.js", HOME);
 }
