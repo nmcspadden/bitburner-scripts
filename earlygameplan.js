bb96a4f70e7ad6dea8b908df73820b60c7e28303
Nick McSpadden <nmcspadden@gmail.com>
2022-02-11 09:02:31 -1000

    Added more content to earlygameplan

--- earlygameplan.js
+++ earlygameplan.js
@@ -1,21 +1,28 @@
 import { commitKarmaFocusedCrime, GANG_KARMA } from "utils/crimes.js";
-import { maximizeScriptUse } from "utils/script_tools.js";
+import { maximizeScriptUse, lookForProcess } from "utils/script_tools.js";
 import { upgradeHome } from "utils/gameplan.js";
 
 /**
- * Early Gameplan w/ Gangs (64 GB RAM)
+ * Early Gameplan w/ Gangs (64+ GB RAM)
 **/
 
-const HOME = 'home';
+const HOME = "home";
 
 /** @param {NS} ns **/
 export async function main(ns) {
 	ns.toast("Starting early game plan!", "info", null);
+	ns.disableLog("ALL"); // Disable the log
+	ns.tail(); // Open a window to view the status of the script
 	// Start crimes until we can do homicides to get to the gang karma, also upgrade home
 	await crimeWhileUpgradingLoop(ns);
 	// Start a gang!
 	await startAGang(ns);
 	// TODO: Join bladeburners if possible
+	ns.print("Kicking off hacking XP scripts");
+	growHackingXP(ns);
+	// Go into a waiting loop where we upgrade, buy programs, re-evaluate hacking XP
+	// TODO: Long term, figure out the right target to hack
+	await upgradingLoop(ns);
 }
 
 /** 
@@ -23,8 +30,6 @@ export async function main(ns) {
  * @param {NS} ns 
 **/
 async function crimeWhileUpgradingLoop(ns) {
-	ns.disableLog("ALL"); // Disable the log
-	ns.tail(); // Open a window to view the status of the script
 	let timeout = 250; // In ms - too low of a time will result in a lockout/hang
 	while (Math.abs(ns.heart.break()) <= GANG_KARMA) {
 		await ns.sleep(timeout); // Wait it out first
@@ -32,7 +37,7 @@ async function crimeWhileUpgradingLoop(ns) {
 		// See if we can upgrade our home
 		upgradeHome(ns);
 		// If we have lots of money, see if we can buy darkweb programs
-		ns.exec('obtainPrograms.js', HOME);
+		ns.exec("obtainPrograms.js", HOME);
 		// Spin up hacking XP tools
 		growHackingXP(ns);
 		// Otherwise, commit crime!
@@ -47,13 +52,14 @@ async function crimeWhileUpgradingLoop(ns) {
 **/
 function growHackingXP(ns) {
 	let HACKSCRIPT;
-	if (ns.getHackingLevel <= 300) {
-		HACKSCRIPT =  "growHackingXP.js";
+	if (ns.getHackingLevel() <= 300) {
+		HACKSCRIPT = "growHackingXP.js";
 	} else {
 		// TODO: Figure out which server to hack
 		HACKSCRIPT = "basicHack.js";
 	}
-	maximizeScriptUse(ns,HACKSCRIPT, HOME);
+	// Run this to 75% of total RAM
+	maximizeScriptUse(ns, HACKSCRIPT, HOME, 75);
 }
 
 /** 
@@ -69,6 +75,13 @@ async function startAGang(ns) {
 		"The Dark Army",
 		"The Syndicate",
 	];
+	// Am I already in a gang? If so, don't need to do anything here
+	ns.print("Checking to see if I'm in a gang...")
+	if (ns.gang.inGang()) {
+		// If I'm already in a gang, kick off the gang script and bail
+		ns.exec("gangs.js", HOME);
+		return
+	}
 	let ready_gang = ns.checkFactionInvitations().find(invite => gangList.includes(invite));
 	while (!ready_gang) {
 		// Wait 30 seconds until the invitations show up
@@ -77,6 +90,31 @@ async function startAGang(ns) {
 		ready_gang = ns.checkFactionInvitations().find(invite => gangList.includes(invite));
 	}
 	let joined = ns.joinFaction(ready_gang);
-	if (joined) ns.print(`Joined ${ready_gang} faction`)
-	ns.exec('gangs.js', HOME);
+	if (joined) ns.print(`Joined ${ready_gang} faction, starting a gang!`)
+	ns.exec("gangs.js", HOME);
+}
+
+/** 
+ * Loop through upgrades, hacking, buying programs, etc.
+ * @param {NS} ns 
+**/
+async function upgradingLoop(ns) {
+	while (ns.getServerMaxRam(HOME) <= 32) {
+		// Make sure gangs is running
+		if (!lookForProcess(ns, HOME, "gangs.js")) ns.exec("gangs.js", HOME)
+		// See if we can upgrade our home
+		ns.print("Looking at home upgrades...");
+		upgradeHome(ns);
+		// If we have lots of money, see if we can buy darkweb programs
+		ns.exec("obtainPrograms.js", HOME);
+		// Create new network map
+		ns.print("Generating updated network map...");
+		ns.exec("utils/networkmap.js", HOME);
+		// Spin up hacking XP tools
+		ns.print("Re-evalauting hacking XP scripts");
+		growHackingXP(ns);
+		// Sleep for 30 seconds
+		ns.print("Sleeping for 30 seconds");
+		await ns.sleep(30000);
+	}
 }
--- starter.js
+++ starter.js
@@ -1,15 +1,26 @@
-/* TO DO: 
-Add RAM checks to see what we start
+/* 
+ Starter.js: starting point for launching the gameplans
 */
 
 const HOME = 'home';
 
 /** @param {NS} ns **/
 export async function main(ns) {
-	// Create network map
-	ns.exec("utils/networkmap.js", HOME);
-	// // Grow hacking XP on joesguns
-	// ns.exec("growHackingXP.js", HOME);
-	// Start early game plan
-	ns.spawn("earlygameplan.js", 1);
+    // Determine our current RAM level
+    let home_ram = ns.getServerMaxRam(HOME);
+    // Create network map
+    ns.exec("utils/networkmap.js", HOME);
+    // We start at 32 GB RAM
+    // TODO: This should be a switch/case statement?
+    if (home_ram <= 32) {
+        // Start early game plan
+        ns.spawn("startinggameplan.js", 1);
+    } else if (home_ram <= 1000) {
+        // Under 1 TB, we're in early game phase
+        ns.spawn('earlygameplan.js', 1);
+    } else {
+        // Haven't figured this one out yet...
+        ns.spawn('midgameplan.js', 1);
+    }
+    // TODO: When do we trigger endgame plan?
 }
