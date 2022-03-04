a0b1acecc9be613aa5caaa1a8227ee095ac25d16
Nick McSpadden <nmcspadden@gmail.com>
2022-03-03 19:30:37 -0800

    Fixing args

--- WIP/growServerEverywhere.js
+++ WIP/growServerEverywhere.js
@@ -3,6 +3,6 @@ import { growTargetServer, findOptimal } from "utils/networkmap.js";
 /** @param {import("../.").NS} ns **/
 export async function main(ns) {
     let target = await findOptimal(ns);
-    if (args[0]) target = args[0];
+    if (ns.args[0]) target = ns.args[0];
     await growTargetServer(ns, target);
 }
