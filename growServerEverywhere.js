02b8df6518823516f6545d8dd58bd14ac28b34fb
Nick McSpadden <nmcspadden@gmail.com>
2022-03-06 20:58:10 -0800

    Moving growServerEverywhere into main

--- growServerEverywhere.js
+++ growServerEverywhere.js
@@ -1,6 +1,6 @@
 import { growTargetServer, findOptimal } from "utils/networkmap.js";
 
-/** @param {import("../.").NS} ns **/
+/** @param {import(".").NS} ns **/
 export async function main(ns) {
     let target = await findOptimal(ns);
     if (ns.args[0]) target = ns.args[0];
